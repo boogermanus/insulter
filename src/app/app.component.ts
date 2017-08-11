3c2270abd11191ad195c38efd9a7db05374eae70
Woodruff <Jordan.Woodruff@tylertech.com>
2017-08-11 17:00:26 -0500

    Basic Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  beginning:string = "Beginning";
+  middle:string = "Middle";
+  end:string = "End"
 }
