48259e400f55505b4c532f7e391a33965f7f08f0
Woodruff <Jordan.Woodruff@tylertech.com>
2018-01-16 16:03:18 -0600

    change material references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdButtonModule} from  '@angular/material';
+import {MatButtonModule} from  '@angular/material';
 import { AppComponent } from './app.component';
 import {DataService} from './data-service';
 import {HttpModule} from '@angular/http';
@@ -14,7 +14,7 @@ import {HttpModule} from '@angular/http';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdButtonModule,
+    MatButtonModule,
     HttpModule
   ],
   providers: [DataService],
