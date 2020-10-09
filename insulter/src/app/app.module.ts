ec5da96081c8b09b7758b1cdedd4ee71ca7ce2ab
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-08 20:27:50 -0500

    trying to improve styling...poorly

--- insulter/src/app/app.module.ts
+++ insulter/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
-import {DataService} from './data-service';
-import {HttpClientModule} from '@angular/common/http';
+import { DataService } from './data-service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
