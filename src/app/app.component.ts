6b0e55fa7b3a7f851a04d26d78c8f086e5f12152
Jordan Woodruff <boogermanus@gmail.com>
2025-10-20 19:29:30 -0500

    #93 - Starting persist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,45 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card'
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { IInsult } from './interfaces/iinsult';
-import { InsultService } from './services/insult.service';
-import { MatButtonModule} from '@angular/material/button';
-import { MatTooltipModule} from '@angular/material/tooltip';
-import {provideHttpClient} from '@angular/common/http';
+import {trigger, state, style, transition, animate} from '@angular/animations';
+import {Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card'
+import {MatCheckboxModule} from '@angular/material/checkbox'
+import {IInsult} from './interfaces/iinsult';
+import {InsultService} from './services/insult.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        MatCardModule,
-        MatCheckboxModule,
-        MatButtonModule,
-        MatTooltipModule,
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    animations: [
-        trigger('isInsultGenerated', [
-            state('true', style({ opacity: 1 })),
-            state('false', style({ opacity: 0 })),
-            transition('0 => 1', animate('250ms')),
-            transition('1 => 0', animate('250ms'))
-        ])
-    ]
+  selector: 'app-root',
+  imports: [
+    MatCardModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('isInsultGenerated', [
+      state('true', style({opacity: 1})),
+      state('false', style({opacity: 0})),
+      transition('0 => 1', animate('250ms')),
+      transition('1 => 0', animate('250ms'))
+    ])
+  ]
 })
 export class AppComponent {
+
   public title = 'insulter';
   public insult: IInsult;
   public insultVisible = false;
   public nsfwInsults = false;
   public insultGenerated = false;
   public timeoutId: any;
-
+  public insultList: IInsult[] = [];
 
   public get fullInsult(): string {
-    return `${this.insult.beginning} ${this.insult.middle} ${this.insult.end}`;
+    return this.getFullInsult(this.insult);
   }
+
   constructor(private _service: InsultService) {
     this.insult = {
       beginning: ' ',
@@ -46,6 +47,7 @@ export class AppComponent {
       end: ' '
     };
   }
+
   public getInsult(): void {
     this.insultGenerated = true;
     // clear any existing timeout to keep the animation in check
@@ -54,6 +56,7 @@ export class AppComponent {
     // call the service and get a new insult
     this.insult = this._service.getInsult(this.nsfwInsults);
     this.insultVisible = true;
+    this.addInsult(this.insult);
 
     // after 3 seconds change the animation and fade out
     this.timeoutId = setTimeout(() => {
@@ -64,5 +67,17 @@ export class AppComponent {
   public switchModes(): void {
     this.nsfwInsults = !this.nsfwInsults;
     this.insultVisible = false;
+  }
+
+  private addInsult(insult: IInsult): void {
+    this.insultList.push(insult);
+
+    if (this.insultList.length > 3) {
+      this.insultList.shift();
+    }
+  }
+
+  private getFullInsult(insult: IInsult): string {
+    return `${insult.beginning} ${insult.middle} ${insult.end}`;
   }
 }
